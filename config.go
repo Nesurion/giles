43f3ec0fb97c389eaaef370d0fe273f7aab2cbdf
Nesurion <maik.schwan@gmail.com>
2016-06-09 12:46:17 +0200

    error handling and config file

---
+++ config.go
@@ -0,0 +1,10 @@
+package main
+
+type Config struct {
+	MovieTempPath   string
+	MovieArchivPath string
+	TvTempPath      string
+	TvArchivPath    string
+	Port            string
+	Mode            string
+}
